9a80d130c467df1789b64f9f118dc2a72d672b73
tcgesan <ethuntcg@gmail.com>
2025-01-09 23:11:10 -0600

    new update

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ document.getElementById("course");
 function hey(){
     window.location.href='./Html/course.html'
 }
+
+
+// const arrowMul = (a,b) => {
+//     console.log(a*b);
+// }
+
+// let a = 20,
+//  b = 10;
+
+// const sumArrow = (a,b) => {
+//     console.log( "The sum is " = a+b)  // ans is 
+// }
+
+
+
+function  vowelFun(str){
+    let count = 0 ;
+for (const char of str){
+if (  char === "a" || char === "e " || char === "i" ||char === "o " || char === "u "){
+    count ++;
+    console.log
+}
+console.log(vowelFun)
+}
+}
